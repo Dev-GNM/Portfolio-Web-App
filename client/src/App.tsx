891ed8fed88616daf91a415a629e81cfdd110de7
Geoffrey Njihia <geoffreynjihia15@gmail.com>
2023-05-13 05:15:14 -0700

    rendering likes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Geoffrey Njihia 
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+
+  
+
+const [like, setLike] = useState([]);
+const [dislike, setDislike] = useState([]);
+
+const [comment, setComment] = useState([]);
+
+const handleLike = (id) => {
+    setLike([...like, id]);
 }
+
+const handleDislike = (id) => {
+    setDislike([...dislike, id]);
+}
+
+const handleComment = (id) => {
+    setComment([...comment, id]);
+}
+
+
+
+
+  return (
+    <div className='App'>
+
+
+
+    </div>
+  )
+};
 
 export default App;
